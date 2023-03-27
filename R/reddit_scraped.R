dfa370361732a43e731a3f66d6151e0ec93d103e
Lina Huang <huan2384@umn.edu>
2023-03-27 09:56:19 -0500

    update vizualization, analysis and publication part of reddit_scraped.R

--- R/reddit_scraped.R
+++ R/reddit_scraped.R
@@ -35,15 +35,19 @@ rstats_tbl <- tibble(
 print(head(rstats_tbl, 25))
 
 #Visualization
-ggplot(rstats_tbl, aes(x = upvotes, y = comments)) +
-  geom_point() +
-  labs(
-    title = "Relationship between Upvotes and Comments",
-    x = "Upvotes",
-    y = "Comments"
-  )
+# Create a scatterplot using ggplot with rstats_tbl as the data source
+ggplot(rstats_tbl, aes(x = upvotes, y = comments)) + # Initialize the ggplot with the data and map upvotes to the x-axis and comments to the y-axis 
+  geom_point() +  # Add a scatterplot layer (geom_point) to display each data point as a point
+  labs(x = "Upvotes", y = "Comments", title = "Relationship between Upvotes and Comments") # Set the axis labels and the plot title
+
 #Analysis
-# Calculate correlation and p-value
-correlation_test <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
-correlation_test$estimate
-correlation_test$p.value
+correlation_test <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments) # Calculate correlation and p-value
+
+#Publication
+#The correlation between upvotes and comments was r(23) = .11, p = .61. This test was not statistically significant.
+p_value <- round(correlation_test$p.value, 2) # Format p values
+correlation <- round(correlation_test$estimate, 2) #correlation
+df <- correlation_test$parameter  # extract df
+significant <- ifelse(p_value < 0.05, "was", "was not") # Check for significance
+# Print the result
+print(sprintf("The correlation between upvotes and comments was r(%d) = .%g, p = .%g. This test %s statistically significant.", df, (correlation * 100) %% 100, (p_value * 100) %% 100, significant))
